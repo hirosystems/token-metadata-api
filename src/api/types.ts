656a7cd503da38814a997fd4609d7da282b2b83b
Rafael Cárdenas <rafael@rafaelcr.com>
2022-07-09 10:50:09 -0500

    feat: more types and validation

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,26 +1,65 @@
 import { Static, Type } from '@sinclair/typebox';
 
-/** SIP-016 Token Metadata */
+/**
+ * SIP-016 Token Metadata
+ */
 export const TokenMetadata = Type.Object({
   sip: Type.Integer(),
   name: Type.String(),
   description: Type.Optional(Type.String()),
   image: Type.Optional(Type.String({ format: 'uri' })),
-  // attributes
+  attributes: Type.Optional(Type.Array(
+    Type.Object({
+      trait_type: Type.String(),
+      value: Type.Union([
+        Type.Object({}),
+        Type.String(),
+        Type.Number(),
+        Type.Integer(),
+        Type.Boolean(),
+        Type.Array(Type.Any()),
+      ]),
+      display_type: Type.Optional(Type.String())
+    })
+  )),
   properties: Type.Optional(Type.Object({})),
   localization: Type.Optional(Type.Object({
     uri: Type.String({ format: 'uri' }),
     default: Type.String(),
-    // locales
-  }))
-});
+    locales: Type.Array(Type.String()),
+  })),
+}, { $id: 'token-metadata' });
+export type TokenMetadataType = Static<typeof TokenMetadata>;
 
+/**
+ * SIP-010 Fungible Token
+ */
 export const FungibleToken = Type.Object({
   name: Type.String(),
   symbol: Type.String(),
   decimals: Type.Integer(),
   total_supply: Type.Integer(),
   token_uri: Type.String({ format: 'uri' }),
-})
+  metadata: Type.Ref(TokenMetadata),
+});
+export type FungibleTokenType = Static<typeof FungibleToken>;
 
-export type FungibleTokenSchema = Static<typeof FungibleToken>
+export const FungibleTokenParams = Type.Object({
+  contract_id: Type.String(),
+});
+export type FungibleTokenParamsType = Static<typeof FungibleTokenParams>;
+
+/**
+ * SIP-009 Non-Fungible Token
+ */
+export const NonFungibleToken = Type.Object({
+  token_uri: Type.String({ format: 'uri' }),
+  metadata: Type.Ref(TokenMetadata),
+});
+export type NonFungibleTokenType = Static<typeof NonFungibleToken>;
+
+export const NonFungibleTokenParams = Type.Object({
+  contract_id: Type.String(),
+  token_id: Type.Integer(),
+});
+export type NonFungibleTokenParamsType = Static<typeof NonFungibleTokenParams>;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Fastify from 'fastify';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
-import { FungibleTokenSchema } from './api/types';
+import { FungibleTokenParams, FungibleTokenParamsType, FungibleTokenType, NonFungibleTokenParams, NonFungibleTokenParamsType, NonFungibleTokenType } from './api/types';
 
 const fastify = Fastify({
   trustProxy: true,
@@ -12,13 +12,35 @@ fastify.get('/', function (request, reply) {
   reply.send({ hello: 'world' });
 });
 
-fastify.get<{ Reply: FungibleTokenSchema }>('/ft/:contract_id', function (request, reply) {
+fastify.get<{ Params: FungibleTokenParamsType, Reply: FungibleTokenType }>('/ft/:contract_id', { schema: { params: FungibleTokenParams } }, (request, reply) => {
+  request.validationError
+  const contractId = request.params.contract_id;
   reply.send({
-    name: 'MiamiCoin',
-    symbol: 'MIA',
+    name: 'NewYorkCityCoin',
+    symbol: 'NYC',
     decimals: 6,
     total_supply: 200,
-    token_uri: 'https://cdn.citycoins.co/metadata/newyorkcitycoin.json'
+    token_uri: 'https://cdn.citycoins.co/metadata/newyorkcitycoin.json',
+    metadata: {
+      sip: 9,
+      name: 'NewYorkCityCoin',
+      description: 'A CityCoin for New York City, ticker is NYC, Stack it to earn Stacks (STX)',
+      image: 'https://cdn.citycoins.co/logos/newyorkcitycoin.png'
+    }
+  });
+});
+
+fastify.get<{ Params: NonFungibleTokenParamsType, Reply: NonFungibleTokenType }>('/nft/:contract_id/:token_id', { schema: { params: NonFungibleTokenParams } }, (request, reply) => {
+  const contractId = request.params.contract_id;
+  const tokenId = request.params.token_id;
+  reply.send({
+    token_uri: 'https://cdn.citycoins.co/metadata/newyorkcitycoin.json',
+    metadata: {
+      sip: 10,
+      name: 'NewYorkCityCoin',
+      description: 'A CityCoin for New York City, ticker is NYC, Stack it to earn Stacks (STX)',
+      image: 'https://cdn.citycoins.co/logos/newyorkcitycoin.png'
+    }
   });
 });
 
