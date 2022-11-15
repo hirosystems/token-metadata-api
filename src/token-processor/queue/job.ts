910d2c020f8a6b9aa031f3e3a95ee2e79f96f0d3
Rafael Cárdenas <rafael@rafaelcr.com>
2022-11-15 15:12:39 -0600

    fix: eslint plugins

--- src/token-processor/queue/job.ts
+++ src/token-processor/queue/job.ts
@@ -1,15 +1,11 @@
 import { PgStore } from "../../pg/pg-store";
 import { DbJob } from "../../pg/types";
-import { JobQueue } from "./job-queue";
 
 export abstract class Job {
   protected readonly db: PgStore;
   protected readonly job: DbJob;
 
-  constructor(args: {
-    db: PgStore;
-    job: DbJob;
-  }) {
+  constructor(args: { db: PgStore; job: DbJob }) {
     this.db = args.db;
     this.job = args.job;
   }
