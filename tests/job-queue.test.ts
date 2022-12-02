332f3d4169ad0bf8a756166e703aabb3d9a36347
Rafael Cárdenas <rafael@rafaelcr.com>
2022-12-02 11:59:33 -0600

    chore: improve test queue

--- tests/job-queue.test.ts
+++ tests/job-queue.test.ts
@@ -5,11 +5,14 @@ import { JobQueue } from '../src/token-processor/queue/job-queue';
 import { cycleMigrations } from './helpers';
 
 class TestJobQueue extends JobQueue {
+  constructor(args: { db: PgStore }) {
+    super(args);
+    this['isRunning'] = true; // Simulate a running queue.
+  }
   async testAdd(job: DbJob): Promise<void> {
     return this.add(job);
   }
   async testAddJobBatch(): Promise<number> {
-    this['isRunning'] = true; // Simulate a running queue.
     return this.addJobBatch();
   }
 }
