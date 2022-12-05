5f1697d875bb2b28ad985cf71065b933f70e7056
Rafael Cárdenas <rafael@rafaelcr.com>
2022-12-05 10:38:29 -0600

    feat: build openapi spec and generate redoc (#53)
    * feat: generate openapi.yaml
    
    * feat: build redoc
    
    * fix: only load swagger in development
    
    * docs: update comments [skip-ci]

--- src/api/init.ts
+++ src/api/init.ts
@@ -1,12 +1,44 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyPluginAsync } from 'fastify';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { FtRoutes } from './routes/ft';
 import { NftRoutes } from './routes/nft';
 import { PgStore } from '../pg/pg-store';
 import FastifyCors from '@fastify/cors';
-import FastifySwagger from '@fastify/swagger';
+import FastifySwagger, { SwaggerOptions } from '@fastify/swagger';
 import { StatusRoutes } from './routes/status';
 import FastifyMetrics from 'fastify-metrics';
+import { Server } from 'http';
+
+export const ApiSwaggerOptions: SwaggerOptions = {
+  openapi: {
+    info: {
+      title: 'Stacks Token Metadata Service',
+      description:
+        'A microservice that indexes metadata for every single Fungible and Non-Fungible Token in the Stacks blockchain and exposes it via REST API endpoints.',
+      version: '0.0.1',
+    },
+    externalDocs: {
+      url: 'https://github.com/rafaelcr/token-metadata-service',
+      description: 'Source Repository',
+    },
+    tags: [
+      {
+        name: 'Tokens',
+        description: 'Token metadata',
+      },
+    ],
+  },
+  exposeRoute: true,
+};
+
+export const Api: FastifyPluginAsync<Record<never, never>, Server, TypeBoxTypeProvider> = async (
+  fastify,
+  options
+) => {
+  await fastify.register(FtRoutes);
+  await fastify.register(NftRoutes);
+  await fastify.register(StatusRoutes);
+};
 
 export async function buildApiServer(args: { db: PgStore }) {
   const fastify = Fastify({
@@ -15,36 +47,13 @@ export async function buildApiServer(args: { db: PgStore }) {
   }).withTypeProvider<TypeBoxTypeProvider>();
 
   fastify.decorate('db', args.db);
-
-  await fastify.register(FastifyCors);
   if (process.env['NODE_ENV'] === 'production') {
     await fastify.register(FastifyMetrics);
+  } else if (process.env['NODE_ENV'] === 'development') {
+    await fastify.register(FastifySwagger, ApiSwaggerOptions);
   }
-  await fastify.register(FastifySwagger, {
-    openapi: {
-      info: {
-        title: 'Stacks Token Metadata Service',
-        description:
-          'A microservice that indexes metadata for every single Fungible and Non-Fungible Token in the Stacks blockchain and exposes it via REST API endpoints.',
-        version: '0.0.1',
-      },
-      externalDocs: {
-        url: 'https://github.com/rafaelcr/token-metadata-service',
-        description: 'Source Repository',
-      },
-      tags: [
-        {
-          name: 'Tokens',
-          description: 'Token metadata',
-        },
-      ],
-    },
-    exposeRoute: true,
-  });
-
-  await fastify.register(FtRoutes);
-  await fastify.register(NftRoutes);
-  await fastify.register(StatusRoutes);
+  await fastify.register(FastifyCors);
+  await fastify.register(Api);
 
   return fastify;
 }
---
+++ util/openapi-generator.ts
@@ -0,0 +1,29 @@
+import Fastify, { FastifyPluginAsync } from 'fastify';
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import { Api, ApiSwaggerOptions } from '../src/api/init';
+import FastifySwagger from '@fastify/swagger';
+import { mkdirSync, writeFileSync } from 'fs';
+import { Server } from 'http';
+
+/**
+ * Generates `openapi.yaml` based on current Swagger definitions.
+ */
+export const ApiGenerator: FastifyPluginAsync<
+  Record<never, never>,
+  Server,
+  TypeBoxTypeProvider
+> = async (fastify, options) => {
+  await fastify.register(FastifySwagger, ApiSwaggerOptions);
+  await fastify.register(Api);
+  mkdirSync('./tmp');
+  writeFileSync('./tmp/openapi.yaml', fastify.swagger({ yaml: true }));
+};
+
+const fastify = Fastify({
+  trustProxy: true,
+  logger: true,
+}).withTypeProvider<TypeBoxTypeProvider>();
+
+void fastify.register(ApiGenerator).then(async () => {
+  await fastify.close();
+});
