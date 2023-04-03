572a6626308b85edfc66202e238964fffdd15ffd
Rafael Cárdenas <rafael@rafaelcr.com>
2023-04-02 19:13:34 -0600

    fix: support FTs with missing metadata but correct token data

--- src/token-processor/queue/job/process-token-job.ts
+++ src/token-processor/queue/job/process-token-job.ts
@@ -114,7 +114,15 @@ export class ProcessTokenJob extends Job {
 
     let metadataLocales: DbMetadataLocaleInsertBundle[] | undefined;
     if (uri) {
-      metadataLocales = await fetchAllMetadataLocalesFromBaseUri(uri, token);
+      try {
+        metadataLocales = await fetchAllMetadataLocalesFromBaseUri(uri, token);
+      } catch (error) {
+        // If the fetch error is retryable, rethrow for job retry. If it's not but we don't have any
+        // data to display otherwise, rethrow so we can mark the job as failed/invalid.
+        if (error instanceof RetryableJobError || !(name || symbol || fDecimals || fTotalSupply)) {
+          throw error;
+        }
+      }
     }
 
     const tokenValues: DbProcessedTokenUpdateBundle = {
