736955451c651f7c5f9d4d985060bfa68dee7b27
Rafael Cárdenas <rafael@rafaelcr.com>
2024-08-27 11:17:18 -0600

    fix: correctly classify image errors vs metadata errors (#251)

--- src/token-processor/util/errors.ts
+++ src/token-processor/util/errors.ts
@@ -95,22 +95,22 @@ export class StacksNodeHttpError extends Error {
 
 export function getUserErrorInvalidReason(error: UserError): DbJobInvalidReason {
   switch (true) {
-    case error instanceof MetadataSizeExceededError:
-      return DbJobInvalidReason.metadataSizeExceeded;
     case error instanceof ImageSizeExceededError:
       return DbJobInvalidReason.imageSizeExceeded;
-    case error instanceof MetadataTimeoutError:
-      return DbJobInvalidReason.metadataTimeout;
+    case error instanceof MetadataSizeExceededError:
+      return DbJobInvalidReason.metadataSizeExceeded;
     case error instanceof ImageTimeoutError:
       return DbJobInvalidReason.imageTimeout;
-    case error instanceof MetadataParseError:
-      return DbJobInvalidReason.metadataParseFailed;
+    case error instanceof MetadataTimeoutError:
+      return DbJobInvalidReason.metadataTimeout;
     case error instanceof ImageParseError:
       return DbJobInvalidReason.imageParseFailed;
-    case error instanceof MetadataHttpError:
-      return DbJobInvalidReason.metadataHttpError;
+    case error instanceof MetadataParseError:
+      return DbJobInvalidReason.metadataParseFailed;
     case error instanceof ImageHttpError:
       return DbJobInvalidReason.imageHttpError;
+    case error instanceof MetadataHttpError:
+      return DbJobInvalidReason.metadataHttpError;
     case error instanceof SmartContractClarityError:
       return DbJobInvalidReason.tokenContractClarityError;
     default:
